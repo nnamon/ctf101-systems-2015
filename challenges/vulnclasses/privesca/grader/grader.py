ff77f99e7794e580b3e60eec3b8ac6bf8aa59c79
amon <amon@nandynarwhals.org>
2015-08-15 15:02:40 +0800

    Updated

--- challenges/vulnclasses/privesca/grader/grader.py
+++ challenges/vulnclasses/privesca/grader/grader.py
@@ -1,5 +1,5 @@
 def grade(arg, key):
-    if "flag{1nn0c3nt_th1nk_4ga1n}".lower() == key.lower():
+    if "flag{3sc4l4t3d_r34lly_qu1ckly}".lower() == key.lower():
         return True, "Just because something's simple doesn't mean it's not " \
             "dangerous."
     else:
